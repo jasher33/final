e5a76f7e19e05ed6e051ac0e3da645b8de0d61d6
jasher33 <jasher33@gmail.com>
2016-07-31 20:40:09 -0700

    asdf

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/',function(req,res){
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
@@ -45,7 +45,7 @@ app.get('/reset-table',function(req,res,next){
     "weight INT,"+
     "date DATE,"+
     "lbs BOOLEAN)";
-    mysql.query(createString, function(err){
+    pool.query(createString, function(err){
       context.results = "Table reset";
       res.render('home'/*,context*/);
     })
