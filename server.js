d3eb032b6d26da2be3a625cf00640d26c5b60b13
jasher33 <jasher33@gmail.com>
2016-08-01 21:27:43 -0700

    asdf

--- server.js
+++ server.js
@@ -78,7 +78,8 @@ app.post('/addNReturn',function(req,res){
 
 app.post('/deleteRow',function(req,res){
 	
-	var context = {};
+	console.log("TPDJKJ");
+	//var context = {};
 	//console.log(req.body.id);
 	/*pool.query('DELETE FROM workouts WHERE id = ?', [req.body.id], function(err, rows, fields){
 		if(err){
